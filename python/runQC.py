08e3c52f4658166d6fa85084963fd14c26115bb6
imnderoo <nderoo.wong@gmail.com>
2013-03-18 10:40:51 -0400

    Stores metrics in dictionary before inserting in DB

--- python/runQC.py
+++ python/runQC.py
@@ -6,6 +6,7 @@ import os, time, sys
 import fileinput
 import argparse
 import xml.etree.ElementTree as ET
+import argparse
 from decimal import *
 from bs4 import BeautifulSoup
 
@@ -14,14 +15,36 @@ def main():
 	parser = argparse.ArgumentParser(description='Script for converting BED files into .interval_lists')
 	parser.add_argument('runID', help='runID')
 	args = parser.parse_args()
-
-	"""Go into /data2/raw/runs/runID/Data/reports/ to extract average:
-		- Cluster Density ("NumClusters By Lane.xml") - Need to be 750K to 800K avg
-		- Cluster Density ("NumClusters By Lane PF.xml") - Need to be above 80% avg
-		- ClustersRaw, ClustersRawSD, ClustersPF, ClustersPFSD, PrcPFClusters, PrcPFClustersDF,
-		- FirstCycleIntPF, FirstCycleIntPFSD, PrcIntensityAfter20CyclesPF, PrcIntensityAfter20CyclsPFSD
-			- All found in (Summary/read2.xml)
-
+	
+	runID = args.runID
+	
+	conn = connect()
+	cursor = conn.cursor()
+	
+	# Create QC Metrics Dictionary: Contains 8 dictionary, 1 for each lane. 
+	# Each dictionary will contain qc metrics
+	qcDict = createQCDict()
+	
+	for 
+	
+	"""
+		Go into /data2/raw/runs/runID/Data/reports/ to extract cluster density
+			- Cluster Density ("NumClusters By Lane.xml") - Need to be 750K to 800K avg
+			- Cluster Density ("NumClusters By Lane PF.xml") - Need to be above 80% avg
+	"""
+		parseClusterDensity(runID, qcDict)
+		
+	""""
+		Go into /data2/raw/runs/runID/Data/reports/ to extract read summary metrics
+		
+		- number of clusters
+		- intensity: 1st cycle
+		- intensity: after 20 cycle
+	"""
+	
+	"""
+		Other metrics that may be used?
+		
 		# - A G C T FWHM - Focus Quality ("Intesnity/Charts_<LaneNum>.xml") - Not included atm
 		# - A G C T Intensity Quality ("Intensity/Charts_<LaneNum>.xml") - Not included atm
 
@@ -30,29 +53,8 @@ def main():
 
 	"""
 
-	clusterDenseFile = "/data2/raw/runs/" + args.runID + "/Data/reports/NumClusters By Lane.xml"
-	tree = ET.parse(clusterDenseFile)
-	root = tree.getroot()
 	
-	for child in root:
-		laneNo = child.attrib['key']
-		clustersDenseMin = child.attrib['min']
-		clustersDenseMax = child.attrib['max']
-		clustersDenseMed = child.attrib['p50']
-
-	# print(laneNo, clustersDenseMin, clustersDenseMax, clustersDenseMed, sep=" ")
-
-	clusterDensePFFile = "/data2/raw/runs/" + args.runID + "/Data/reports/NumClusters By Lane PF.xml"
-	tree= ET.parse(clusterDensePFFile)
-	root = tree.getroot()
-
-	for child in root:
-		laneNo = child.attrib['key']
-		clustersDensePFMin = child.attrib['min']
-		clustersDensePFMax = child.attrib['max']
-		clustersDensePFMed = child.attrib['p50']		
-
-	# print(laneNo, clustersDensePFMin, clustersDensePFMax, clustersDensePFMed, sep=" ")
+def parseReadSummary(runID, qcDict):
 
 	readSummaryFile = "/data2/raw/runs/" + args.runID + "/Data/reports/Summary/read2.xml"
 
@@ -90,4 +92,62 @@ def main():
 	
 	outfile.close()
 	"""
+	
+def createQCDict():
+	qcDict = {}
+	
+	for lane in range(1,9):
+		qcDict[lane]={}
+
+	return qcDict
+		
+def parseClusterDensity(runID, qcDict)
+
+	clusterDenseFile = "/data2/raw/runs/" + runID + "/Data/reports/NumClusters By Lane.xml"
+	tree = ET.parse(clusterDenseFile)
+	root = tree.getroot()
+	
+	for child in root:
+		lane = child.attrib['key'] # May need to convert from String to Int
+
+		qc_Dict[lane]['clusters_dense_min'] = child.attrib['min']
+		qc_Dict[lane]['clusters_dense_max'] = child.attrib['max']
+		qc_Dict[lane]['clusters_dense_med'] = child.attrib['p50']
+
+	# print(laneNo, clustersDenseMin, clustersDenseMax, clustersDenseMed, sep=" ")
+
+	clusterDensePFFile = "/data2/raw/runs/" + runID + "/Data/reports/NumClusters By Lane PF.xml"
+	tree= ET.parse(clusterDensePFFile)
+	root = tree.getroot()
+
+	for child in root:
+		lane = child.attrib['key']
+		qc_Dict[lane]['clusters_dense_pf_min'] = child.attrib['min']
+		qc_Dict[lane]['clusters_dense_pf_max'] = child.attrib['max']
+		qc_Dict[lane]['clusters_dense_pf_med'] = child.attrib['p50']		
+
+	# print(laneNo, clustersDensePFMin, clustersDensePFMax, clustersDensePFMed, sep=" ")
+	
+	
+def parseReadSummary(runID, qcDict)
+
+	
+
+def execute_insert(conn, cursor, sql):
+	try:
+		cursor.execute(sql)
+		conn.commit()
+	except pymysql.Error as e: 
+                # Ignore duplication errors
+		print ("Error when executing", sql, ":", e, sep=" ")
+		conn.rollback()
+
+def connect():
+        try:
+                conn = pymysql.connect(host="localhost", user="pipeline", passwd="2Tilapia", db="analysis")
+                return conn
+        except pymysql.Error as e:
+                print ("Error connecting to DB", e.args[0], sep=" ")
+                sys.exit(1)
+
 main()

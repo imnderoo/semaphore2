b5ca142dfafe9cd75ffd4972c95f901f6ffaac2b
imnderoo <nderoo.wong@gmail.com>
2013-03-18 11:29:39 -0400

    Add metrics from read summary into dictionary

--- python/runQC.py
+++ python/runQC.py
@@ -40,9 +40,17 @@ def main():
 		- number of clusters
 		- intensity: 1st cycle
 		- intensity: after 20 cycle
-	"""
+	"""	
+		parseReadSummary(runID, qcDict)
+	
+		# To get the flowCellID, split by "_", take the last element [-1] and strip first letter [1:]
+		flowCellID = args.runID.split("_")[-1][1:]
+	
+		demultiFile = "/data2/analysis/" + runID + "/BaseCall_Stats_" + flowCellID + "/Demultiplex_Stats.htm"
+		# soup = BeautifulSoup(open(demultiFile))		
 	
 	"""
+		Future TO-DO:
 		Other metrics that may be used?
 		
 		# - A G C T FWHM - Focus Quality ("Intesnity/Charts_<LaneNum>.xml") - Not included atm
@@ -52,7 +60,16 @@ def main():
 			- Tile, Cycle, Read level metrics
 
 	"""
+	
+def createQCDict():
+	qcDict = {}
 
+	# Create 8 dictionary entries. 1 for each lane. 
+	for lane in range(1,9):
+		# Each lane will be its own dictionary of values.	
+		qcDict[lane]={}
+
+	return qcDict
 	
 def parseReadSummary(runID, qcDict):
 
@@ -62,47 +79,27 @@ def parseReadSummary(runID, qcDict):
 	root = tree.getroot()
 
 	for child in root:
-		laneNo = child.attrib['key']
-		clustersRaw_Mean = child.attrib['ClustersRaw']
-		clustersRaw_SD = child.attrib['ClustersRawSD']
-		clustersPF_Mean = child.attrib['ClustersPF']
-		clustersPF_SD = child.attrib['ClustersPFSD']
-		pctClustersPF_Mean = child.attrib['PrcPFClusters']
-		pctClustersPF_SD = child.attrib['PrcPFClustersSD']
-		firstCycleIntensePF_Mean = child.attrib['FirstCycleIntPF']
-		firstCycleIntensePF_SD = child.attrib['FirstCycleIntPFSD']
-		pctIntense20CyclePF_Mean = child.attrib['PrcIntensityAfter20CyclesPF']
-		pctIntense20CyclePF_SD = child.attrib['PrcIntensityAfter20CyclesPFSD']
+		lane = child.attrib['key']
+
+		qc_Dict[lane]['clusters_raw_mean'] = child.attrib['ClustersRaw']
+		qc_Dict[lane]['clusters_raw_sd'] = child.attrib['ClustersRawSD']
+		qc_Dict[lane]['clusters_pf_mean'] = child.attrib['ClustersPF']
+		qc_Dict[lane]['clusters_pf_sd'] = child.attrib['ClustersPFSD']
+		qc_Dict[lane]['pct_clusters_pf_mean'] = child.attrib['PrcPFClusters']
+		qc_Dict[lane]['pct_clusters_pf_sd'] = child.attrib['PrcPFClustersSD']
+		qc_Dict[lane]['first_cycle_intense_pf_mean'] = child.attrib['FirstCycleIntPF']
+		qc_Dict[lane]['first_cycle_intense_pf_sd'] = child.attrib['FirstCycleIntPFSD']
+		qc_Dict[lane]['pct_intense20cycle_pf_mean'] = child.attrib['PrcIntensityAfter20CyclesPF']
+		qc_Dict[lane]['pct_intense20cycle_pf_sd'] = child.attrib['PrcIntensityAfter20CyclesPFSD']
 
 
 	# print (laneNo, clustersRaw_Mean, clustersRaw_SD, clustersPF_Mean, clustersPF_SD, sep="  ")
 	# print (laneNo, pctClustersPF_Mean, pctClustersPF_SD, firstCycleIntensePF_Mean, firstCycleIntensePF_SD, pctIntense20CyclePF_Mean, pctIntense20CyclePF_SD, sep="  ")
-
-	# To get the flowCellID, split by "_", take the last element [-1] and strip first letter [1:]
-	flowCellID = args.runID.split("_")[-1][1:]
 	
-	demultiFile = "/data2/analysis/" + args.runID + "/BaseCall_Stats_" + flowCellID + "/Demultiplex_Stats.htm"
-	soup = BeautifulSoup(open(demultiFile))		
-	
-	
-	"""
-	outfile = open(outfileName, 'w')
-			
-		outfile.write('\t'.join(lineCol) + '\n')
-	
-	outfile.close()
-	"""
-	
-def createQCDict():
-	qcDict = {}
-	
-	for lane in range(1,9):
-		qcDict[lane]={}
-
-	return qcDict
 		
 def parseClusterDensity(runID, qcDict)
 
+	# Parse all cluster dense
 	clusterDenseFile = "/data2/raw/runs/" + runID + "/Data/reports/NumClusters By Lane.xml"
 	tree = ET.parse(clusterDenseFile)
 	root = tree.getroot()
@@ -114,8 +111,7 @@ def parseClusterDensity(runID, qcDict)
 		qc_Dict[lane]['clusters_dense_max'] = child.attrib['max']
 		qc_Dict[lane]['clusters_dense_med'] = child.attrib['p50']
 
-	# print(laneNo, clustersDenseMin, clustersDenseMax, clustersDenseMed, sep=" ")
-
+	# Parse cluster dense that passed filter (PF)
 	clusterDensePFFile = "/data2/raw/runs/" + runID + "/Data/reports/NumClusters By Lane PF.xml"
 	tree= ET.parse(clusterDensePFFile)
 	root = tree.getroot()
@@ -124,14 +120,7 @@ def parseClusterDensity(runID, qcDict)
 		lane = child.attrib['key']
 		qc_Dict[lane]['clusters_dense_pf_min'] = child.attrib['min']
 		qc_Dict[lane]['clusters_dense_pf_max'] = child.attrib['max']
-		qc_Dict[lane]['clusters_dense_pf_med'] = child.attrib['p50']		
-
-	# print(laneNo, clustersDensePFMin, clustersDensePFMax, clustersDensePFMed, sep=" ")
-	
-	
-def parseReadSummary(runID, qcDict)
-
-	
+		qc_Dict[lane]['clusters_dense_pf_med'] = child.attrib['p50']			
 
 def execute_insert(conn, cursor, sql):
 	try:
